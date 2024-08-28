ececdd8b1694660abfabe9775945926f5cc79804
8ctopus <hello@octopuslabs.io>
2024-08-28 10:13:36 +0400

    Release 1.1.0

--- src/EntryPoint.php
+++ src/EntryPoint.php
@@ -10,6 +10,6 @@ $file = '/vendor/autoload.php';
 
 require file_exists(__DIR__ . $file) ? __DIR__ . $file : dirname(__DIR__) . $file;
 
-$app = new Application('hstat', '1.0.2');
+$app = new Application('hstat', '1.1.0');
 $app->add(new CommandSpeed());
 $app->run();
